bd6c2ea1d6a44505f50c8bdc7ba0ac268ca8e172
Neyra Marzouk <nmarzouk@vermeg.com>
2022-06-27 11:56:37 +0100

    update

--- app/src/main/java/com/example/vermegrecycleview/MainActivity.java
+++ app/src/main/java/com/example/vermegrecycleview/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.vermegrecycleview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -11,17 +12,22 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     RecyclerView rc;
     List<Contact> ListData = new ArrayList<>();
+    ContactAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rc=findViewById(R.id.recycleContact);
+        adapter=new ContactAdapter(MainActivity.this,ListData);
+        rc.setLayoutManager(new LinearLayoutManager(this));
+        rc.setAdapter(adapter);
+        getData();
     }
     void getData(){
         ListData.add(new Contact("neyra","marzouk","52636354",R.mipmap.download));
         ListData.add(new Contact("iheb","farhat","52636354",R.mipmap.download));
         ListData.add(new Contact("siwar","dandana","52636354",R.mipmap.download));
         ListData.add(new Contact("omar","tajine","52636354",R.mipmap.download));
-
+        adapter.notifyDataSetChanged();
     }
 }
